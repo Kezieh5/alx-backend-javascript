1e605c945565887054586a6229073b775c9def5e
Kezieh5 <mwangikezzieh@gmail.com>
2023-11-30 21:48:31 -0800

    all commits

--- 0x02-ES6_classes/.eslintrc.js
+++ 0x02-ES6_classes/.eslintrc.js
@@ -25,11 +25,13 @@ module.exports = {
       'LabeledStatement',
       'WithStatement',
     ],
+    'max-classes-per-file': ['error', 2],
+    'no-underscore-dangle': ['error', { 'allow': ['_range', '_size', '_location', '_year', '_firstName', '_lastName', '_holbertonClass', '_brand', '_motor', '_color', '_maxStudentsSize'] }],
   },
-  overrides:[
+  overrides: [
     {
       files: ['*.js'],
       excludedFiles: 'babel.config.js',
-    }
-  ]
+    },
+  ],
 };
